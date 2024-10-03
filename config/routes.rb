ab05bce80fb4a78640cd67d7d79472a1d642801f
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-10-03 18:37:33 +0200

    Event controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   root to: 'events#index' # Par exemple, la page d'accueil pointe vers l'index des événements
 
   # Ajoute cette ligne pour Devise
---
+++ test/system/events_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+
+class EventsTest < ApplicationSystemTestCase
+  setup do
+    @event = events(:one)
+  end
+
+  test "visiting the index" do
+    visit events_url
+    assert_selector "h1", text: "Events"
+  end
+
+  test "should create event" do
+    visit events_url
+    click_on "New event"
+
+    fill_in "Date", with: @event.date
+    fill_in "Description", with: @event.description
+    fill_in "Estimated guests", with: @event.estimated_guests
+    fill_in "Event type", with: @event.event_type
+    fill_in "Name", with: @event.name
+    fill_in "User", with: @event.user_id
+    click_on "Create Event"
+
+    assert_text "Event was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Event" do
+    visit event_url(@event)
+    click_on "Edit this event", match: :first
+
+    fill_in "Date", with: @event.date
+    fill_in "Description", with: @event.description
+    fill_in "Estimated guests", with: @event.estimated_guests
+    fill_in "Event type", with: @event.event_type
+    fill_in "Name", with: @event.name
+    fill_in "User", with: @event.user_id
+    click_on "Update Event"
+
+    assert_text "Event was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Event" do
+    visit event_url(@event)
+    click_on "Destroy this event", match: :first
+
+    assert_text "Event was successfully destroyed"
+  end
+end
