d0eed82ea951703dd8f1869858613ca3415f575c
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-10-09 16:16:17 +0200

    Allow null values for category_id in tasks table

---
+++ db/migrate/20241009141534_change_category_id_in_tasks_to_allow_null.rb
@@ -0,0 +1,5 @@
+class ChangeCategoryIdInTasksToAllowNull < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :tasks, :category_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_09_141118) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_09_141534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_141118) do
     t.string "assigned_to_email"
     t.bigint "event_id", null: false
     t.bigint "guest_id"
-    t.bigint "category_id", null: false
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
