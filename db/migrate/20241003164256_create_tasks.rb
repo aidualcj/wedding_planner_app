9ec0799cc872f2d1ea2f6c4196bbd357c77a5e84
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-10-09 16:12:17 +0200

    Allow null values for guest_id in tasks table

--- db/migrate/20241003164256_create_tasks.rb
+++ db/migrate/20241003164256_create_tasks.rb
@@ -8,7 +8,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
       t.string :assigned_to_name
       t.string :assigned_to_email
       t.references :event, null: false, foreign_key: true
-      t.references :guest, null: false, foreign_key: true
+      t.references :guest, null: false, foreign_key: true, null: true
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
