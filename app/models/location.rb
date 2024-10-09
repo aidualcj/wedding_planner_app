c0084a1fee4b24fdeeb8ef5b9e0663adf790bb80
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-10-09 16:47:23 +0200

    Add presence validation for location_type and capacity in Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
   belongs_to :event
+
+  # Validation for location_type
+  validates :location_type, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
